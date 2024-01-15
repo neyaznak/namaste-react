d98ab62f627068a55e71ebbcce2a64bea91a2275
MohammadNAKnw <neyaznak@gmail.com>
2024-01-15 23:13:02 +0530

    JSX

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is Namaste React!!!"),
-    React.createElement("h2", {}, "By Akshay Saini"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag!"),
-    React.createElement("h2", {}, "I'm an h2 tag!"),
-  ]),
-]);
+const heading = (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX🚀
+  </h1>
+);
 
-
-console.log(parent);
+//React Component
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(heading);
