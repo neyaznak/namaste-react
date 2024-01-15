f7e6f4aaf2cfd629aebf6b44503ecfecf9d2c2b2
MohammadNAKnw <neyaznak@gmail.com>
2024-01-15 22:00:33 +0530

    igniting the app

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag!"),
-    React.createElement("h2", {}, "I'm an h2 tag!"),
+    React.createElement("h1", {}, "This is Namaste React!!!"),
+    React.createElement("h2", {}, "By Akshay Saini"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm an h1 tag!"),
@@ -15,15 +18,3 @@ console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!"
-// );
-
-// console.log(heading);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
---
+++ README.md
@@ -0,0 +1,21 @@
+# Namaste React
+
+
+# parcel
+- Dev Build
+- Local Server
+- HMR =Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Build
+- Image Optimization
+- Minification of files
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - removes unused code
+- Different dev and prod build
