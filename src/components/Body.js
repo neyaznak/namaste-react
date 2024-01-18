cf6169c7b816afc534dde8b9c49aabfb15fc3319
MohammadNAKnw <neyaznak@gmail.com>
2024-01-18 15:46:50 +0530

    login logout toggle btn - using useState hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,38 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-    return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+    //Optional
+    setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }
+
+  //conditional rendering
+
+    return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+    ) : (
       <div className="body">
         <div className="filter">
           <button
             className="filter-btn"
             onClick={() => {
               const filteredList = listOfRestaurants.filter(
-                (res) => res.info.avgRating > 4
+                (res) => res.info.avgRating > 4.3
               );
               setListOfRestaurants(filteredList);
             }}
@@ -27,6 +47,7 @@ const Body = () => {
         </div>
       </div>
     );
+
   };
 
   export default Body;
