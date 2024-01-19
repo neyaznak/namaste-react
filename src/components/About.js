754eec531b2e755c1cd1faa9920ba30faaf6eac8
MohammadNAKnw <neyaznak@gmail.com>
2024-01-19 21:16:04 +0530

    setState in class comps for updating state vars

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,6 @@ const About = () => {
     <div>
       <h1>About Us</h1>
       <h2>This is Namaste React Web Series</h2>
-      <User name={"Neyaz Khan (function)"}/>
-
       <UserClass name={"Neyaz Khan (class)"} location={"Bokaro Class"}/>
     </div>
   );
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,18 +1,32 @@
 import React from "react";
 
 class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
-
-        console.log(props);
-    }
+    this.state = {
+      count: 0,
+    };
+  }
 
   render() {
+    const { name, location } = this.props;
+    const { count } = this.state;
+
     return (
       <div className="user-card">
-        <h2>Name: {this.props.name}</h2>
-        <h3>Location: {this.props.location}</h3>
+        <h1>Count: {count}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Count Increase
+        </button>
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
         <h4>Name: @neyaznak</h4>
       </div>
     );
