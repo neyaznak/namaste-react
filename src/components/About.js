ea834d68dd1a8a0454ed74a57f2787c895d36449
MohammadNAKnw <neyaznak@gmail.com>
2024-01-19 22:45:08 +0530

    lifecycle of react class components

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,30 @@
+import { Component } from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <h2>This is Namaste React Web Series</h2>
-      <UserClass name={"Neyaz Khan (class)"} location={"Bokaro Class"}/>
-    </div>
-  );
-};
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount(){
+    console.log("Parent Component Did Mount")
+  }
+
+  render() {
+    console.log("Parent Render");
+    return (
+      <div>
+        <h1>About Us</h1>
+        <h2>This is Namaste React Web Series</h2>
+        <UserClass name={"First "} location={"Bokaro Class"} />
+        <UserClass name={"Second "} location={"Delhi Class"} />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,17 +1,27 @@
-import React from "react";
+import { Component } from "react";
 
-class UserClass extends React.Component {
+class UserClass extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       count: 0,
     };
+
+    console.log(this.props.name+ "Child Constructor");
+  }
+
+  componentDidMount(){
+    console.log(this.props.name+ "Child Component Did Mount")
+
+    //Api call
   }
 
   render() {
     const { name, location } = this.props;
     const { count } = this.state;
+
+    console.log(this.props.name+ "Child Render");
 
     return (
       <div className="user-card">
@@ -27,7 +37,7 @@ class UserClass extends React.Component {
         </button>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
-        <h4>Name: @neyaznak</h4>
+        <h4>Contact: @neyaznak</h4>
       </div>
     );
   }
