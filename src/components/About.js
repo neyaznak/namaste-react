83f675532d8849bc55aa1b3b2831c6a63bbdb62e
MohammadNAKnw <neyaznak@gmail.com>
2024-01-19 20:35:59 +0530

    class based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div>
       <h1>About Us</h1>
       <h2>This is Namaste React Web Series</h2>
+      <User name={"Neyaz Khan (function)"}/>
+
+      <UserClass name={"Neyaz Khan (class)"} location={"Bokaro Class"}/>
     </div>
   );
 };
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = ({name}) => {
+  return (
+    <div className="user-card">
+      <h2>Name: {name}</h2>
+      <h3>Location: Bokaro</h3>
+      <h4>Name: @neyaznak</h4>
+    </div>
+  );
+};
+
+export default User;
