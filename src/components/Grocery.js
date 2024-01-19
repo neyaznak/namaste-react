0e7036c748b21f3fae154f44e61b69403f1c6abf
MohammadNAKnw <neyaznak@gmail.com>
2024-01-20 02:28:02 +0530

    chunking, code splitting, dynamic bundling, lazy loading, on demand loading, dynamic import

---
+++ src/components/Grocery.js
@@ -0,0 +1,9 @@
+const Grocery = () => {
+  return (
+    <h1>
+      Our Online Grocery Store and we have a lot of child components inside this
+      web page!!!
+    </h1>
+  );
+};
+export default Grocery
