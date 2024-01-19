4658ffa1a185862b956bc01dc84ca064e8231759
MohammadNAKnw <neyaznak@gmail.com>
2024-01-20 04:09:49 +0530

    tailwind css

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -16,20 +16,16 @@ const RestaurantCard = (props) => {
   
     return (
       <div
-        className="res-card"
-        style={{
-          backgroundColor: "#f0f0f0",
-        }}
-      >
+        className="m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200">
         <img
-          className="res-logo"
+          className="rounded-lg"
           alt="res-logo"
           src={
             CDN_URL +
             resData.info.cloudinaryImageId
           }
         />
-        <h3>{name}</h3>
+        <h3 className="font-bold py-4 text-lg">{name}</h3>
         <h4>{cuisines.join(", ")}</h4>
         <h4>{costForTwo}</h4>
         <h4>{avgRating} stars</h4>
